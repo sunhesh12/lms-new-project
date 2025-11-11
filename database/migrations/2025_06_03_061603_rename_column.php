909ce1b850bb004a7d6b50e236bc460b96a12e2b
RasuwanK <kalharaweragala@gmail.com>
2025-11-11 17:45:05 +0530

    Commented unwanted renaming

--- database/migrations/2025_06_03_061603_rename_column.php
+++ database/migrations/2025_06_03_061603_rename_column.php
@@ -11,18 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('portal_users', function (Blueprint $table) {
-            $table->renameColumn('Full_name', 'full_name');
-            $table->renameColumn('Age', 'age');
-            $table->renameColumn('Email', 'email');
-            $table->renameColumn('Mobile_No', 'mobile_no');
-            $table->renameColumn('Address', 'address');
-            $table->renameColumn('Profile_Picture', 'profile_picture');
-            $table->renameColumn('Password', 'password');
-            $table->renameColumn('Role', 'role');
-            $table->renameColumn('Status', 'status');
+        // Schema::table('portal_users', function (Blueprint $table) {
+        //     $table->renameColumn('Full_name', 'full_name');
+        //     $table->renameColumn('Age', 'age');
+        //     $table->renameColumn('Email', 'email');
+        //     $table->renameColumn('Mobile_No', 'mobile_no');
+        //     $table->renameColumn('Address', 'address');
+        //     $table->renameColumn('Profile_Picture', 'profile_picture');
+        //     $table->renameColumn('Password', 'password');
+        //     $table->renameColumn('Role', 'role');
+        //     $table->renameColumn('Status', 'status');
 
-        });
+        // });
     }
 
     /**
