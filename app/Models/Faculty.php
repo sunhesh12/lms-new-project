831901293b47737ab3f5f859957fb1ae67d2b2d4
sunhesh12 <hsdbandaranayake@gmail.com>
2025-11-18 12:01:11 +0530

    update Faculty migrations for db

---
+++ app/Models/Faculty.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Faculty extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'fac_name',
+        'theme',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    
+}
