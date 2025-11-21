ef8460f081e83c872dfac5e57b0891687fd74d68
RasuwanK <kalharaweragala@gmail.com>
2025-11-21 14:21:26 +0530

    Added new test user

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -27,21 +27,35 @@ class UsersTableSeeder extends Seeder
         for ($i = 0; $i < 10; $i++) {
 
             $users[] = [
-                'name'            => $faker->name(),
-                'email'           => $faker->unique()->safeEmail(),
-                'user_phone_no'   => $faker->numerify('07########'), // SL-style phone number
-                'profile_pic'     => 'profile/default.png',
-                'user_dob'        => $faker->date('Y-m-d'),
-                'address'         => $faker->address(),
-                'status'          => $i % 2 == 0 ? 'blocked' : 'active',
-                'faculty_id'      => $faker->randomElement($facultysIds),   // Assuming faculties table has IDs 1–4
-                'password'        => Hash::make('password123'),
-                'created_at'      => now(),
-                'updated_at'      => now(),
-                'course_id'       => $faker->randomElement($courseIds), // Assuming courses table has IDs 1–3
+                'name' => $faker->name(),
+                'email' => $faker->unique()->safeEmail(),
+                'user_phone_no' => $faker->numerify('07########'), // SL-style phone number
+                'profile_pic' => 'profile/default.png',
+                'user_dob' => $faker->date('Y-m-d'),
+                'address' => $faker->address(),
+                'status' => $i % 2 == 0 ? 'blocked' : 'active',
+                'faculty_id' => $faker->randomElement($facultysIds),   // Assuming faculties table has IDs 1–4
+                'password' => Hash::make('password123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'course_id' => $faker->randomElement($courseIds), // Assuming courses table has IDs 1–3
             ];
         }
 
         DB::table('users')->insert($users);
+
+        DB::table('users')->insert([
+            'name' => 'Test User',
+            'user_phone_no' => $faker->numerify('0705085269'), // SL-style phone number
+            'profile_pic' => 'profile/default.png',
+            'user_dob' => $faker->date('Y-m-d'),
+            'address' => $faker->address(),
+            'status' => $i % 2 == 0 ? 'blocked' : 'active',
+            'faculty_id' => $faker->randomElement($facultysIds),   // Assuming faculties table has IDs 1–4
+            'password' => Hash::make('password123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'course_id' => $faker->randomElement($courseIds),
+        ]);
     }
 }
