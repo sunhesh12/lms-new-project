789cb6dfa886d7049551a925bab53991762eedcc
RasuwanK <kalharaweragala@gmail.com>
2025-12-02 13:06:08 +0530

    Decoupling rendering logic

---
+++ resources/js/components/ItemList.jsx
@@ -0,0 +1,18 @@
+import styles from "@/css/components/topic-item-list.module.css";
+
+export default function ItemList({ items, render }) {
+    return (
+        <div id="topic-item-list" className={styles.topicItemContainer}>
+            <div id="vertical-line" className={styles.verticalLine}></div>
+            <ul id="item-list" className={styles.itemList}>
+                {items.map(topicItem => {
+                    return (
+                        <li id="item">
+                            {render(topicItem)}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
+}
