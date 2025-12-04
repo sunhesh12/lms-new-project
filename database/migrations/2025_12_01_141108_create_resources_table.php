ded26c7005176beecaf5dc69e616c0e5edfdb003
RasuwanK <kalharaweragala@gmail.com>
2025-12-04 18:46:14 +0530

    Column modification

--- database/migrations/2025_12_01_141108_create_resources_table.php
+++ database/migrations/2025_12_01_141108_create_resources_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('resources', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
             $table->timestamps();
-            $table->string('url'); 
+            $table->string('url');
             $table->string('caption');
             $table->boolean('is_deleted')->default(false);
         });
