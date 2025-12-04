05044b85f41376a3ff2cd1c290ad299ee6dc7645
RasuwanK <kalharaweragala@gmail.com>
2025-12-04 18:44:55 +0530

    Adding fillable columns

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Resource extends Model
 {
-    /** @use HasFactory<\Database\Factories\ResourceFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'topic_id',
+        'url',
+        'caption',
+        'is_deleted',
+    ];
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
 }
